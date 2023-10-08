1717aad59674e1648db6f210b82a740de2a54332
srijanid <srijanidas06@gmail.com>
2023-10-08 16:10:48 +0530

    commit

--- app/src/main/java/com/adds/safewomen/MainActivity.java
+++ app/src/main/java/com/adds/safewomen/MainActivity.java
@@ -1,6 +1,14 @@
 package com.adds.safewomen;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
